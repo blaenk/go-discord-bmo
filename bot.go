f6be84d184212549273325b778a3b89e9330de80
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-11-26 17:44:40 -0800

    move most code to Bot

---
+++ bot.go
@@ -0,0 +1,232 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"path"
+	"strconv"
+	"sync"
+
+	"github.com/bwmarrin/dgvoice"
+	"github.com/bwmarrin/discordgo"
+	ivona "github.com/jpadilla/ivona-go"
+	"github.com/mvdan/xurls"
+)
+
+// Bot is a representation of the Bot.
+type Bot struct {
+	ownerID         string
+	userID          string
+	ivonaClient     *ivona.Ivona
+	lock            sync.Mutex
+	session         *discordgo.Session
+	voiceStateCache map[string]*discordgo.VoiceState
+}
+
+// New creates a new Bot.
+func New() *Bot {
+	return &Bot{
+		voiceStateCache: make(map[string]*discordgo.VoiceState),
+		ivonaClient:     ivona.New(os.Getenv("IVONA_ACCESS_KEY"), os.Getenv("IVONA_SECRET_KEY")),
+	}
+}
+
+// Close closes the Discord session.
+func (b *Bot) Close() error {
+	return b.session.Close()
+}
+
+// Open opens the Discord session.
+func (b *Bot) Open() error {
+	var err error
+
+	b.session, err = discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+
+	if err != nil {
+		log.Fatal("Couldn't establish a Discord session", err)
+	}
+
+	b.registerHandlers()
+
+	return b.session.Open()
+}
+
+func (b *Bot) registerHandlers() {
+	b.session.AddHandler(b.onReady)
+	b.session.AddHandler(b.onMessageCreate)
+	b.session.AddHandler(b.onVoiceStateUpdate)
+}
+
+func (b *Bot) getSelfID() {
+	if botUser, err := b.session.User("@me"); err == nil {
+		b.userID = botUser.ID
+	} else {
+		log.Fatal("Couldn't obtain account details", err)
+	}
+}
+
+func (b *Bot) getOwnerID() {
+	if ownerUser, err := b.session.User(os.Getenv("BOT_OWNER")); err == nil {
+		b.ownerID = ownerUser.ID
+	} else {
+		log.Fatal("Couldn't obtain account details", err)
+	}
+}
+
+func (b *Bot) onReady(_ *discordgo.Session, event *discordgo.Ready) {
+	log.Println("Connection is ready")
+
+	b.getSelfID()
+	b.getOwnerID()
+}
+
+// IsSelf checks if the ID is the bot's ID.
+func (b *Bot) IsSelf(ID string) bool {
+	return b.userID == ID
+}
+
+// IsOwner checks if the ID is the bot's owner's ID.
+func (b *Bot) IsOwner(ID string) bool {
+	return b.ownerID == ID
+}
+
+func (b *Bot) onMessageCreate(_ *discordgo.Session, msg *discordgo.MessageCreate) {
+	// Ignore messages we created.
+	if b.IsSelf(msg.Author.ID) {
+		log.Println("Skipping self message:\n", msg.Content)
+		return
+	}
+
+	b.previewURLs(msg.Message)
+
+	if b.IsOwner(msg.Author.ID) {
+		log.Println("Received message from owner")
+
+		b.respondToPing(msg.Message)
+	}
+}
+
+func (b *Bot) respondToPing(msg *discordgo.Message) {
+	if msg.Content == "ping" {
+		_, _ = b.session.ChannelMessageSend(msg.ChannelID, "Pong!")
+	}
+}
+
+func (b *Bot) previewURLs(msg *discordgo.Message) {
+	for _, link := range xurls.Relaxed.FindAllString(msg.Content, -1) {
+		parsed, err := url.Parse(link)
+
+		if err != nil {
+			log.Println("Couldn't parse link:", link, err)
+		}
+
+		b.previewHackerNews(msg, parsed)
+	}
+}
+
+func (b *Bot) previewHackerNews(msg *discordgo.Message, link *url.URL) {
+	if link.Host == "news.ycombinator.com" {
+		return
+	}
+
+	id := link.Query().Get("id")
+
+	intID, err := strconv.Atoi(id)
+
+	if err != nil {
+		log.Println("Couldn't convert ID", id, "to int:", err)
+		return
+	}
+
+	item := getHNItem(intID)
+
+	_, _ = b.session.ChannelMessageSend(msg.ChannelID, item.Format())
+}
+
+func (b *Bot) getIvonaSpeech(text string) (string, error) {
+	shaSum := fmt.Sprintf("%x", sha1.Sum([]byte(text)))
+	speechPath := path.Join("./data/speech", shaSum)
+
+	if _, err := os.Stat(speechPath); err == nil {
+		log.Println("Found cached speech for:", text)
+
+		return speechPath, nil
+	}
+
+	log.Println("No cached speech found; requesting from Ivona.")
+
+	speechOptions := ivona.NewSpeechOptions(text)
+	response, err := b.ivonaClient.CreateSpeech(speechOptions)
+
+	if err != nil {
+		return "", err
+	}
+
+	if ioutil.WriteFile(speechPath, response.Audio, 0644) != nil {
+		return "", err
+	}
+
+	return speechPath, nil
+}
+
+func (b *Bot) ivonaSpeak(voiceConnection *discordgo.VoiceConnection, text string) error {
+	if speechFile, err := b.getIvonaSpeech(text); err == nil {
+		dgvoice.PlayAudioFile(voiceConnection, speechFile)
+	} else {
+		return err
+	}
+
+	return nil
+}
+
+func (b *Bot) speakPresenceUpdate(voiceState *discordgo.VoiceState, action string) {
+	log.Println("User", voiceState.UserID, action, "channel", voiceState.ChannelID)
+
+	voiceConnection, err := b.session.ChannelVoiceJoin(voiceState.GuildID, voiceState.ChannelID, false, true)
+
+	user, err := b.session.User(voiceState.UserID)
+
+	if err != nil {
+		log.Fatal("Can't find user", voiceState.UserID)
+	}
+
+	presenceText := fmt.Sprintf("%s %s the channel", user.Username, action)
+
+	if err := b.ivonaSpeak(voiceConnection, presenceText); err != nil {
+		log.Println("Couldn't speak with Ivona:", err)
+	}
+}
+
+func (b *Bot) onVoiceStateUpdate(_ *discordgo.Session, update *discordgo.VoiceStateUpdate) {
+	if b.IsSelf(update.UserID) {
+		log.Println("Ignoring bot's VoiceStateUpdate")
+		return
+	}
+
+	if cached, ok := b.voiceStateCache[update.UserID]; ok {
+		changedChannels := cached.ChannelID != update.ChannelID
+
+		if !changedChannels {
+			log.Println("No channel change detected")
+			return
+		}
+
+		leftChannel := cached.ChannelID != ""
+
+		if leftChannel {
+			b.speakPresenceUpdate(cached, "left")
+		}
+	}
+
+	joinedChannel := update.ChannelID != ""
+
+	if joinedChannel {
+		b.speakPresenceUpdate(update.VoiceState, "joined")
+	}
+
+	b.voiceStateCache[update.UserID] = update.VoiceState
+}
--- main.go
+++ main.go
@@ -1,183 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"github.com/bwmarrin/dgvoice"
-	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
-	"github.com/jpadilla/ivona-go"
-	// "github.com/layeh/gopus"
-	"crypto/sha1"
-	"github.com/mvdan/xurls"
-	"io/ioutil"
+
 	"log"
-	"net/url"
 	"os"
 	"os/signal"
-	"path"
-	"strconv"
-	"sync"
 )
-
-// Bot is a representation of the Bot.
-type Bot struct {
-	lock            sync.Mutex
-	OwnerID         string
-	UserID          string
-	VoiceStateCache map[string]*discordgo.VoiceState
-}
-
-func NewBot(UserID, OwnerID string) *Bot {
-	return &Bot{
-		OwnerID:         OwnerID,
-		UserID:          UserID,
-		VoiceStateCache: make(map[string]*discordgo.VoiceState),
-	}
-}
-
-var BMO *Bot
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	sess, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	bot := New()
 
-	if err != nil {
-		log.Fatal("Couldn't establish a Discord session", err)
-	}
-
-	botUser, err := sess.User("@me")
-
-	if err != nil {
-		log.Fatal("Couldn't obtain account details", err)
-	}
-
-	ownerUser, err := sess.User(os.Getenv("BOT_OWNER"))
-
-	BMO = NewBot(botUser.ID, ownerUser.ID)
-
-	sess.AddHandler(Ready)
-	sess.AddHandler(MessageCreate)
-	sess.AddHandler(VoiceStateUpdate)
-
-	err = sess.Open()
-	defer sess.Close()
-
-	if err != nil {
-		log.Fatal("Couldn't open a connection to Discord", err)
-	}
-
-	log.Println("Bot is now running")
+	bot.Open()
+	defer bot.Close()
 
 	// Listen for SIGINT and gracefully disconnect.
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, os.Interrupt)
 
 	<-signalChannel
-}
-
-func Ready(sess *discordgo.Session, event *discordgo.Ready) {
-	log.Println("Connection is ready")
-}
-
-func IvonaSpeak(text string) (string, error) {
-	ivonaClient := ivona.New(os.Getenv("IVONA_ACCESS_KEY"), os.Getenv("IVONA_SECRET_KEY"))
-	speechOptions := ivona.NewSpeechOptions(text)
-
-	response, err := ivonaClient.CreateSpeech(speechOptions)
-
-	if err != nil {
-		return "", err
-	}
-
-	shaSum := fmt.Sprintf("%x", sha1.Sum([]byte(text)))
-
-	speechPath := path.Join("./data/speech", shaSum)
-
-	if ioutil.WriteFile(speechPath, response.Audio, 0644) != nil {
-		return "", err
-	}
-
-	return speechPath, nil
-}
-
-func VoiceStateUpdate(sess *discordgo.Session, update *discordgo.VoiceStateUpdate) {
-	if update.UserID == BMO.UserID {
-		log.Println("Ignoring bot VoiceStateUpdate")
-		return
-	}
-
-	log.Printf("VoiceStateUpdate: %+v", update.VoiceState)
-
-	cached, ok := BMO.VoiceStateCache[update.UserID]
-
-	if ok && cached.ChannelID != update.ChannelID && cached.ChannelID != "" {
-		log.Println("User", cached.UserID, "left channel", cached.ChannelID)
-	}
-
-	if update.ChannelID != "" {
-		log.Println("User", update.UserID, "joined channel", update.ChannelID)
-
-		user, err := sess.User(update.UserID)
-
-		if err != nil {
-			log.Fatal("Can't find user", update.UserID)
-		}
-
-		speechFile, err := IvonaSpeak(fmt.Sprintf("%s joined the channel", user.Username))
-
-		voiceConnection, err := sess.ChannelVoiceJoin(update.GuildID, update.ChannelID, false, true)
-
-		dgvoice.PlayAudioFile(voiceConnection, speechFile)
-	}
-
-	BMO.VoiceStateCache[update.UserID] = update.VoiceState
-}
-
-func MessageCreate(sess *discordgo.Session, msg *discordgo.MessageCreate) {
-	// Ignore messages we created.
-	if msg.Author.ID == BMO.UserID {
-		log.Println("Skipping self message:\n", msg.Content)
-		return
-	}
-
-	// if msg.Author.ID == os.Getenv("BOT_OWNER") {
-	if msg.Author.ID == BMO.OwnerID {
-		log.Println("Received message from owner")
-
-		if msg.Content == "ping" {
-			_, _ = sess.ChannelMessageSend(msg.ChannelID, "Pong!")
-		}
-
-		if msg.Content == "disconnect" {
-			_, _ = sess.ChannelMessageSend(msg.ChannelID, "Disconnecting!")
-			sess.Close()
-		}
-	}
-
-	for _, link := range xurls.Relaxed.FindAllString(msg.Content, -1) {
-		parsed, err := url.Parse(link)
-
-		if err != nil {
-			log.Println("Couldn't parse link:", link, err)
-		}
-
-		switch parsed.Host {
-		case "news.ycombinator.com":
-			id := parsed.Query().Get("id")
-
-			intID, err := strconv.Atoi(id)
-
-			if err != nil {
-				log.Println("Couldn't convert ID", id, "to int:", err)
-				break
-			}
-
-			item := getHNItem(intID)
-
-			_, _ = sess.ChannelMessageSend(msg.ChannelID, item.Format())
-		}
-	}
 }
